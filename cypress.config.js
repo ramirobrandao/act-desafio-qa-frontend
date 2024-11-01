d632b592750d53bb05d040e866439b744ba864a1
Ramiro Brandão <ramirobrandao@outlook.com>
2024-11-01 00:14:07 -0300

    Instalando biblioteca Mochawesome para geração de relatórios

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,21 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     //acesso ao site/ambiente
-    baseUrl: 'https://front.serverest.dev/'
+    baseUrl: 'https://front.serverest.dev/',
+    reporter: 'mochawesome',
+    screenshotOnRunFailure: true,
+    "reporterOptions": {
+      "reportDir": "cypress/reports/mochawesome-report",
+      "overwrite": false,
+      "html": true,
+      "json": true,
+      "charts": true,
+      "reportFilename": "report",
+      "timestamp": "mmddyyyy_HHMMss",
+      "inlineAssets": true,
+      "autoOpen": true,
+      "reportPageTitle": "Relatório de execução de testes frontend",
+      "embeddedScreenshots": true
+    }
   },
 });
